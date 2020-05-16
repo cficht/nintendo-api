f15846deaa20945bfc9525bdcca8e2f6b6f78938
cficht <chris.ficht@gmail.com>
2020-05-15 17:34:50 -0700

    Added search by name route

--- db/data-helpers.js
+++ db/data-helpers.js
@@ -14,14 +14,10 @@ afterAll(() => {
 const prepare = model => JSON.parse(JSON.stringify(model));
 const prepareAll = models => models.map(prepare);
 
-// reading our models directory
 const files = fs.readdirSync('./lib/models');
 const getters = files
-  // for each file in our models directory import the model
   .map(file => require(`../lib/models/${file}`))
-  // make sure that what we imported is actually a model
   .filter(Model => Model.prototype instanceof mongoose.Model)
-  // for each model create a getModelName function that returns an instance of our model
   .reduce((acc, Model) => {
     return {
       ...acc,
